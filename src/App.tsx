6c4fa9b88590f67248998f2d2718191f25f2f821
ISHOWO ASMAA KHADIJAH <asmaaishowo2912@gmail.com>
2024-06-06 21:07:04 +0100

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Spices from "../components/spices";
 import Seasoning from "../components/seasoning";
